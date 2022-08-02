9a87c74600c37edb74500a203c77f718d7f2eca7
bbeomgeun <miki308@naver.com>
2022-08-02 23:53:38 +0900

    Test Controller

---
+++ src/main/java/com/example/springdockertest/SpringdockerTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdockertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdockerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdockerTestApplication.class, args);
+    }
+
+}
